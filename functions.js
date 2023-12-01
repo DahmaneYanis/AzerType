5a7c12de15dc642002438b2852bef66b701e075f
d_yanis <dahmane.yanis.2004@gmail.com>
2023-12-01 15:25:17 +0100

    Improve the control of the timer

--- functions.js
+++ functions.js
@@ -8,15 +8,47 @@ function updateScore(){
     baliseScore.innerHTML = score;
 }
 
-function updateTimer(){
+function setTimer(time){
     let baliseTimer = document.getElementById("timer");
-    time--;
     baliseTimer.innerHTML = ` ${Math.floor(time/60)}min${time%60}sec `;
     if (time == 0) {
+        console.log("Fin");
         clearInterval(interval);
     }
+    console.log(time, baliseTimer);
 }
 
-function minToSec(min, sec){
+function updateTimer(){
+    console.log("update");
+    if (time > 0) {
+        setTimer(time--);
+    } else { 
+        clearInterval(interval);
+    }   
+}
+
+function minToSec(min, sec = 0){
     return min*60 + sec;
+}
+
+function initialize(){
+    time = minToSec(1, 1);
+    setTimer(time--);
+}
+
+
+function startTimer(){
+    interval = setInterval(updateTimer, 1000);
+}
+
+function hideButtonStart(){
+    let buttonStart = document.getElementById("start");
+    buttonStart.removeEventListener("click", start);
+    buttonStart.remove();
+}
+
+function start(){
+    initialize();
+    hideButtonStart();
+    startTimer();
 }
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //Timer handling
-time = minToSec(1, 25);
-interval = setInterval(() => {updateTimer()}, 1000);
-console.log(interval);
+let buttonStart = document.getElementById("start");
+buttonStart.addEventListener("click", start);
 
 // Button incrementation
-let button = document.getElementById("increment");
-button.addEventListener("click", () => updateScore(shot, succes));
+// let button = document.getElementById("increment");
+// button.addEventListener("click", () => updateScore(shot, succes));
